b80772481839c8e857da3218fc9921dcc6a3a15f
Velvet233 <57743903+Velvet233@users.noreply.github.com>
2019-11-29 13:38:28 +0800

    Create crawl11-29.py
    mini爬虫test
    出现各种报错的情况
    爬取网页中的title

---
+++ crawl11-29.py
@@ -0,0 +1,25 @@
+//mini爬虫
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
+
+def get_page_title(url):
+    try:
+        html=urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bso=BeautifulSoup(html.read())
+        title=bso.title
+    except AttributeError as e:
+        return None
+    return title
+    
+url='https://www.baidu.com/'
+title=get_page_title(url)
+
+if title==None:
+    print("There is no Title in the page,Oh No!!!")
+else:
+    print(title)
