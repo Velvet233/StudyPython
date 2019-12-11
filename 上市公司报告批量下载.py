1b2c613fdb375a5a1b56e6ec0583b4b1dcda5499
Velvet233 <57743903+Velvet233@users.noreply.github.com>
2019-12-11 14:23:28 +0800

    Create 上市公司报告批量下载.py

---
+++ 上市公司报告批量下载.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def get_and_download_pdf():
+    #we need to add referer to show that we are inside the web
+    response=requests.get('http://query.sse.com.cn/security/stock/queryCompanyBulletin.do?jsonCallBack=jsonpCallback97604&isPagination=true&productId=&keyWord=&securityType=0101&reportType2=&reportType=ALL&beginDate=2019-01-01&endDate=2019-01-30&pageHelp.pageSize=25&pageHelp.pageCount=50&pageHelp.pageNo=1&pageHelp.beginPage=1&pageHelp.cacheSize=1&pageHelp.endPage=5&_=1576040173383p.endPage=5&_=1576038378952',
+    headers={'Referer':'http://www.sse.com.cn/disclosure/listedinfo/announcement/'})
+
+    string_to_be_loaded=response.text[19:-1]
+
+    formatted_data=json.loads(string_to_be_loaded)#一系列的数据
+    for every_report in formatted_data['result']:
+        pdf_url="http://static.sse.com.cn/"+every_report['URL']
+        print(pdf_url)
+        file_name=every_report['TITLE']+'.pdf'
+        resource = requests.get(pdf_url,stream=True)#下载
+        with open(file_name,'wb') as temp:
+            for chunk in resource.iter_counter(1024):
+                temp.write(chunk)
+            print("上市公司报告:"+file_name+",已经完成下载")
+
+#外部调用程序
+if __name__=="__main__" :
+    get_and_download_pdf()
